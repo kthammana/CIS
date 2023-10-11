025f47c1ff912a84099a0e5481cfd1cb56bae5f4
Keerthana Thammana <keerthana.thammana@gmail.com>
2023-10-11 02:48:04 -0400

    Switched to svd least squares for calibration

--- CartesianMath/PivotCalibration.py
+++ CartesianMath/PivotCalibration.py
@@ -18,22 +18,31 @@ def pivotCalibration(G):
     t_Gs = F_G.p.coords.transpose()[..., np.newaxis]
     for i in range(1, G.shape[0]):
         Gt = G[i].transpose()
-        # G0 = np.mean(Gt, axis=1).reshape((-1,1))
         g = Gt - G0
         F_G = registrationArunMethod(g.transpose(), G[i], "G")
-        # print(F_G.R)
-        # print(np.matmul(F_G.R, g[:,0]) + F_G.p.coords)
-        # print(Gt[:,0])
         print(np.linalg.det(F_G.R))
         R_G = np.hstack((F_G.R, negI))
         R_Gs = np.vstack((R_Gs, R_G))
         t_Gs = np.vstack((t_Gs, F_G.p.coords.transpose()[..., np.newaxis]))
     t_Gs = t_Gs * -1
-    # x = np.matmul((np.matmul(np.matmul(R_Gs.transpose(), R_Gs), R_Gs.transpose())), t_Gs)
-    x = np.matmul(np.matmul(np.linalg.inv(np.matmul(R_Gs.transpose(), R_Gs)), R_Gs.transpose()), t_Gs)
-    print(x)
+
+    # regular least squares
+    # x = np.matmul(np.matmul(np.linalg.inv(np.matmul(R_Gs.transpose(), R_Gs)), R_Gs.transpose()), t_Gs)
+    # print(x)
     # print(np.matmul(F_G.R,x[0:3]) + F_G.p.coords.transpose()[..., np.newaxis])
     # print(x[3:6])
+
+    # singular value decomposition least squares
+    # seems to work better than regular least squares
+    # gives correct answer for only file a
+    U, S, Vt = np.linalg.svd(R_Gs, full_matrices=True)
+    zeros = np.zeros((6,30))
+    Sinv = np.hstack((np.linalg.inv(np.diag(S)), zeros))
+    print(Sinv.shape)
+    print(U.transpose().shape)
+    y = np.matmul(np.matmul(Sinv, U.transpose()), t_Gs)
+    x = np.matmul(Vt.transpose(), y)
+    print(x)
 
 
 G = read_empivot("./PA1 Student Data/pa1-debug-a-empivot.txt")
