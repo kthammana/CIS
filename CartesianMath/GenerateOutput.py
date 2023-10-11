9eeef0f589d51bd565c310e13c1f5cf0b75c0d46
Kiana Bronder <kianabronder@kianas-mbp.acasmart.jh.edu>
2023-10-11 13:02:45 -0400

    added file for unknown outputs

--- CartesianMath/GenerateOutput.py
+++ CartesianMath/GenerateOutput.py
@@ -1,7 +1,6 @@
 import numpy as np
 from Point3d import Point3d
-from Frame import Frame
-from FileIO import read_calbody, read_calreadings, read_empivot, read_optpivot, read_output
+from FileIO import read_calbody, read_calreadings, read_empivot, read_optpivot
 from Registration import registrationArunMethod
 
 # output file:
@@ -9,17 +8,31 @@ from Registration import registrationArunMethod
     # P_EM
     # P_opt
     # C_1 through C_N_frames
-d, a, c = read_calbody("../PA1 Student Data/pa1-unknown-h-calbody.txt")
-D, A, C = read_calreadings("../PA1 Student Data/pa1-unknown-h-calreadings.txt")
-G = read_empivot("../PA1 Student Data/pa1-unknown-h-empivot.txt")
-D,H = read_optpivot("../PA1 Student Data/pa1-unknown-h-optpivot.txt")
-f = open("h-outputs.txt", "x")
 
-for i in range(D.shape[0]): # N_frames
+# Calculate P_EM
+d, a, c = read_calbody("../PA1 Student Data/pa1-unknown-h-calbody.txt")
+G = read_empivot("../PA1 Student Data/pa1-unknown-h-empivot.txt")
+P_EM = pivotCalibration(G)
+
+# Calculate P_opt
+D,H = read_optpivot("../PA1 Student Data/pa1-unknown-h-optpivot.txt")
+P_opt = opticalCalibration(d,D,H)
+
+# Calculate C_exp and write all variables to output file
+D, A, C = read_calreadings("../PA1 Student Data/pa1-unknown-h-calreadings.txt")
+filename = "h-output1.txt"
+f = open("../PA1 Outputs/"+filename, "a")
+N_frames = D.shape[0]
+N_C = c.shape[0]
+f.write(N_C, N_frames, filename)
+f.write(P_EM.__str__())
+f.write(P_opt.__str__())
+for i in range(N_frames): # N_frames
     F_D = registrationArunMethod(d, D[i], "D")
     F_A = registrationArunMethod(a, A[i], "A")
     F_DA = F_D.inverse() * F_A
-    for j in range(c.shape[0]): # N_C
+    for j in range(N_C): # N_C
         C0_exp = np.matmul(F_DA.R, c[j].transpose()[..., np.newaxis]) + F_DA.p.coords.transpose()[..., np.newaxis]
-        
+        point = Point3d("EM",C0_exp[0][0], C0_exp[1][0], C0_exp[2][0])
+        f.write(point.__str__())
 f.close()
