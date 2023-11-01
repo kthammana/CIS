55f00ad9e9fc8e510f2e1443d7a77041a8f2d359
Kiana Bronder <kianabronder@kianas-mbp.acasmart.jh.edu>
2023-11-01 10:51:51 -0400

    added reg test

--- PROGRAMS/testing2.py
+++ PROGRAMS/testing2.py
@@ -21,13 +21,41 @@ for i in range(D.shape[0]):
         C_expected[i][j] = [C0_exp[0][0], C0_exp[1][0], C0_exp[2][0]]
 
 coef, q_min, q_max = calcDistortionCorrection(np.vstack(C_expected), np.vstack(C), 3)
+C_errors = np.zeros(C.shape[0:2]) # stores error of each frame
+for i in range(C.shape[0]): # N_frames
+    for j in range(C.shape[1]): # N_C
+        # P_C = Point3d(C[i][j])
+        P_Cexp = Point3d("C", C_exp[i][j])
+        C_errors[i][j] = P_Cexp.error(C[i][j])
+print('Average calibration error per point:', np.mean(C_errors), 'mm')
+
+### DEBUGGING REGISTRATION
+# use given d and artificial D
+artiD = np.array([[0.3404, 0.5853, 0.2238],[-100.2885, -80.1432, 76.7532], 
+              [111.0513, -62.0671, 79.7082],[10.4224, -142.7955, 156.2376],
+              [11.1531, -109.2218, -101.3906],[-89.4758, -189.9503, -24.8612],
+              [121.8640, -171.8742, -21.9062],[21.2351, -252.6026, 54.6232]])
+artiF_D = registrationArunMethod(d, artiD, "D")
+# artificial R and p calculated on MATLAB
+artiR = np.array([[0.0721, 0.7381, -0.6709],[-0.7320, -0.4177, -0.5382],[-0.6774, 0.5299, 0.5102]])
+print('Calculated R:', artiF_D.R)
+print('Expected R:',artiR)
+print('Calculated p:',artiF_D.p.__str__())
+print('Expected p: 0.3404, 0.5853, 0.2238')
+reg_error = 0
+for i in range(3):
+    temp_point = Point3d("D",F_D.R[i])
+    reg_error += temp_point.error(artiR[i])
+    # print(i,':',reg_error)
+reg_error += artiF_D.p.error([0.3404, 0.5853, 0.2238])
+print('Registration error:',reg_error)
 
 # EM pivot calibration
 G = read_empivot("PA1 Student Data/pa1-debug-g-empivot.txt")
 G_corr = np.empty(G.shape)
 for i in range(G_corr.shape[0]):
     G_corr[i] = correctDistortion(G[i], coef, q_min, q_max, 3)
-print(G)
+# print(G)
 P_em_exp = pivotCalibration(G_corr)
 print('EM Calculated output:', P_em_exp.__str__())
 print('EM Expected output:',P_em)
