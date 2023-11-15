b5e8b911409db983168ebed98170452b07ab9254
Kiana Bronder <kianabronder@Kianas-MBP.hsd1.md.comcast.net>
2023-11-15 08:07:47 -0500

    tree done

--- PROGRAMS/Mesh.py
+++ PROGRAMS/Mesh.py
@@ -1,7 +1,6 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
+# import matplotlib.pyplot as plt
+# from mpl_toolkits.mplot3d import Axes3D
 from FileIO import read_mesh, read_output3, read_samplereadings, read_probbody
 from Registration import registrationArunMethod
 from ClosestPointOnTriangle import findClosestPointOnTriangle
@@ -33,138 +32,95 @@ class Mesh(object):
         t_v = self.v_inds[t_idx]
         return [self.v_coors[int(t_v[0])].transpose(), self.v_coors[int(t_v[1])].transpose(), self.v_coors[int(t_v[2])].transpose()]
     
-# can add functions as necessary
-# can change variable names to given V,i,n if that is easier\
+    def calcCentroid(self, vertices):
+        x = 0
+        y = 0
+        z = 0
+        for i in range(3):
+            x += vertices[i][0]
+            y += vertices[i][1]
+            z += vertices[i][2]
+        x /= 3
+        y /= 3
+        z /= 3
+        return np.array([x,y,z])
 
-    # WIP NEED TO MODIFY
 # The following code is contributed by Prajwal Kandekar, as available on
 # GeeksforGeeks and has been modified as necessary to accomodate the Mesh class
 
 # Number of dimensions
 k = 3
 
+'''
+Using a generic KD tree starter code, I modified it so that the node's point
+is the centroid of its triangle and gave it a triangle variable to then hold
+the vertices' coordinate information. To find the nearest node, the given point
+is compared to each triangle's centroid to find the nearest one, then I call
+findClosestPointOnTriangle(...) to calculate the true distance of the point
+from the triangle, rather than distance from the centroid.
+'''
 # A structure to represent node of kd tree
 class Node:
-	def __init__(self, point):
-		self.point = point
-		self.left = None
-		self.right = None
-
-# A method to create a node of K D tree
-def newNode(point):
-	return Node(point)
+	def __init__(self, point, triangle):
+            self.point = point # centroid of triangle
+            self.triangle = triangle
+            self.left = None
+            self.right = None
 
 # Inserts a new node and returns root of modified tree
 # The parameter depth is used to decide axis of comparison
-def insertRec(root, point, depth):
-	# Tree is empty?
-	if not root:
-		return newNode(point)
+def insert(root, point, triangle, depth):
+    if not root: # Empty tree
+        return Node(point, triangle)
 
-	# Calculate current dimension (cd) of comparison
-	cd = depth % k
+    cd = depth % k
 
-	# Compare the new point with root on current dimension 'cd'
-	# and decide the left or right subtree
-	if point[cd] < root.point[cd]:
-		root.left = insertRec(root.left, point, depth + 1)
-	else:
-		root.right = insertRec(root.right, point, depth + 1)
+    if point[cd] < root.point[cd]:
+        root.left = insert(root.left, point, triangle, depth + 1)
+    else:
+        root.right = insert(root.right, point, triangle, depth + 1)
 
-	return root
+    return root
 
-# Function to insert a new point with given point in
-# KD Tree and return new root. It mainly uses above recursive
-# function "insertRec()"
-def insert(root, point):
-	return insertRec(root, point, 0)
+def distance_squared(point1, point2):
+    return sum((p1 - p2) ** 2 for p1, p2 in zip(point1, point2))
 
-# A utility method to determine if two Points are same
-# in K Dimensional space
-def arePointsSame(point1, point2):
-	# Compare individual coordinate values
-	for i in range(k):
-		if point1[i] != point2[i]:
-			return False
+def searchTree(root, point, depth, best_node, best_distance):
+    if not root or root is None:
+        return best_node, best_distance
 
-	return True
+    cd = depth % k
+    next_best = best_node # None
+    next_best_distance = best_distance
 
-# Searches a Point represented by "point[]" in the K D tree.
-# The parameter depth is used to determine current axis.
-def searchRec(root, point, depth):
-	# Base cases
-	if not root:
-		return False
-	if arePointsSame(root.point, point):
-		return True
+    if point[cd] < root.point[cd]:
+        next_best, next_best_distance = searchTree(root.left, point, depth + 1, next_best, next_best_distance)
+    else:
+        next_best, next_best_distance = searchTree(root.right, point, depth + 1, next_best, next_best_distance)
 
-	# Current dimension is computed using current depth and total
-	# dimensions (k)
-	cd = depth % k
+    current_distance = distance_squared(root.point, point)
 
-	# Compare point with root with respect to cd (Current dimension)
-	if point[cd] < root.point[cd]:
-		return searchRec(root.left, point, depth + 1)
+    if current_distance < next_best_distance:
+        next_best = root
+        next_best_distance = current_distance
 
-	return searchRec(root.right, point, depth + 1)
+    other_child = root.right if (point[cd] < root.point[cd]) else root.left
+    if other_child:
+        other_child_distance = (point[cd] - root.point[cd]) ** 2
+        if other_child_distance < next_best_distance:
+            next_best, next_best_distance = searchTree(other_child, point, depth + 1, next_best, next_best_distance)
 
-# Searches a Point in the K D tree. It mainly uses
-# searchRec()
+    return next_best, next_best_distance
+
+# Uses 
 def search(root, point):
-	# Pass current depth as 0
-	return searchRec(root, point, 0)
-
-def plot_kd_tree_3d(node, ax, xmin, xmax, ymin, ymax, zmin, zmax, depth=0):
-    if node is not None:
-        cd = depth % k
-
-        if cd == 0:
-            ax.plot([node.point[0], node.point[0]], [ymin, ymax], [zmin, zmax], color='black', linestyle='-', linewidth=1)
-            plot_kd_tree_3d(node.left, ax, xmin, node.point[0], ymin, ymax, zmin, zmax, depth + 1)
-            plot_kd_tree_3d(node.right, ax, node.point[0], xmax, ymin, ymax, zmin, zmax, depth + 1)
-        elif cd == 1:
-            ax.plot([xmin, xmax], [node.point[1], node.point[1]], [zmin, zmax], color='black', linestyle='-', linewidth=1)
-            plot_kd_tree_3d(node.left, ax, xmin, xmax, ymin, node.point[1], zmin, zmax, depth + 1)
-            plot_kd_tree_3d(node.right, ax, xmin, xmax, node.point[1], ymax, zmin, zmax, depth + 1)
-        else:
-            ax.plot([xmin, xmax], [ymin, ymax], [node.point[2], node.point[2]], color='black', linestyle='-', linewidth=1)
-            plot_kd_tree_3d(node.left, ax, xmin, xmax, ymin, ymax, zmin, node.point[2], depth + 1)
-            plot_kd_tree_3d(node.right, ax, xmin, xmax, ymin, ymax, node.point[2], zmax, depth + 1)
-
-
-def plot_points_3d(ax, points):
-    points = np.array(points)
-    ax.scatter(points[:, 0], points[:, 1], points[:, 2], color='red', marker='o', label='Points')
-
-def main():
-    points = [[3, 6, 1], [17, 15, 0], [13, 15, 6], [6, 12, 4], [9, 1, 2], [2, 7, 3], [10, 19, 5]]
-    root = None
-
-    for point in points:
-        root = insertRec(root, point, 0)
-
-    xmin, xmax = min(p[0] for p in points) - 1, max(p[0] for p in points) + 1
-    ymin, ymax = min(p[1] for p in points) - 1, max(p[1] for p in points) + 1
-    zmin, zmax = min(p[2] for p in points) - 1, max(p[2] for p in points) + 1
-
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-
-    plot_kd_tree_3d(root, ax, xmin, xmax, ymin, ymax, zmin, zmax)
-    plot_points_3d(ax, points)
-
-    ax.set_xlabel('X-axis')
-    ax.set_ylabel('Y-axis')
-    ax.set_zlabel('Z-axis')
-    ax.set_title('3D k-d Tree Visualization')
-    ax.legend()
-    plt.show()
+    nearest_node, __ = searchTree(root, point, 0, None, float('inf'))
+    return nearest_node
 
 def calcDistance(x, y):
     return np.sqrt((x[0] - y[0])**2 + (x[1] - y[1])**2 + (x[2] - y[2])**2)
 
 if __name__ == '__main__':
-    main()
     print("PA3 Output Errors:")
     
     dataset = "PA345 Student Data/PA3-F-Debug"
@@ -192,19 +148,22 @@ if __name__ == '__main__':
     # for PA3, F_reg = 1
     # TO-DO: construct octree/kdtree for searching for points
     # linear search to find the closest points to d_k
+    root = None
+    
+    # Run time should now be O(n) instead of O(n^2)
+    for i in range(ind.shape[0]):
+        verts = mesh.getVerticesOfTriangle(i)
+        root = insert(root, mesh.calcCentroid(verts), verts, 0)
+    
     c_k = np.empty((a.shape[0], 3))
     for i in range(d_k.shape[0]):
-        shortest_dist = np.infty
-        for j in range(ind.shape[0]):
-            c = findClosestPointOnTriangle(d_k[i], mesh.getVerticesOfTriangle(j))
-            dist = calcDistance(d_k[i], c)
-            if dist <= shortest_dist:
-                closest_point = c
-                shortest_dist = dist
-        c_k[i] = closest_point
+        nearest_node = search(root, d_k[i])
+        c_k[i] = findClosestPointOnTriangle(d_k[i], nearest_node.triangle)
+        shortest_dist = calcDistance(d_k[i], c_k[i])
         c_error += calcDistance(c_exp[i], c_k[i])
         mag_error += (np.abs(mag[i]-shortest_dist))
-
+    
+    # Slight difference in these values
     print("d_k error: ", d_error/d_k.shape[0])
     print("c_k error: ", c_error/c_k.shape[0])
     print("mag error: ", mag_error/c_k.shape[0])
